03d697beeb4dd0d9534f0bff48016ccfe1fe95c8
SHEFF48 <sun-service@ukr.net>
2024-02-07 23:45:27 +0200

    Mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -19,19 +19,19 @@ const MobileMenu: FC<IMobileMenu> = ({
   return (
     <div
       className={cn(
-        "container flex flex-col justify-between items-center min-h-[calc(100vh-120px)] pt-[11px] pb-[30px]",
+        "container flex flex-col justify-between items-center gap-[50px] min-h-[calc(100vh-120px)] md:pt-[11px] pb-[30px]",
         className
       )}
     >
       <SearchBar className="w-full lg:hidden" />
       <MenuList
         data={mainMenuList}
-        className="flex flex-col text-[18px] font-medium text-left leading-[140%] gap-[58px] text-[#2A86E5] dark:text-[#0DD0BA]"
+        className="flex flex-col text-[18px] font-medium text-left leading-[140%] gap-[28px] md:gap-[58px] text-[#2A86E5] dark:text-[#0DD0BA]"
         clickHandler={updateValue}
       />
       <MenuList
         data={topMenuList}
-        className="flex flex-row justify-evenly gap-[40px]"
+        className="flex flex-col md:flex-row justify-evenly gap-[23px] md:gap-[40px]"
         clickHandler={updateValue}
       />
     </div>
