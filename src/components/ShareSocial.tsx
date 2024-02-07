3ddc908b7536bd86f4ec6742851ff3a670a334a7
SHEFF48 <sun-service@ukr.net>
2024-02-08 01:09:11 +0200

    Widget fixed

--- src/components/ShareSocial.tsx
+++ src/components/ShareSocial.tsx
@@ -9,19 +9,19 @@ interface IShareSocial {
 const ShareSocial: FC<IShareSocial> = ({ className }) => {
   return (
     <div className={cn("", className)}>
-      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#3A579A] ">
+      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#3A579A] cursor-pointer ">
         <Facebook className="text-white h-[20px] w-[20px]" />
       </div>
-      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#00ABF0] ">
+      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#00ABF0] cursor-pointer">
         <Twitter className="text-white h-[20px] w-[20px]" />
       </div>
-      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#24A2E0] ">
+      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#24A2E0] cursor-pointer">
         <Instagram className="text-white h-[20px] w-[20px]" />
       </div>
-      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#FC461E] ">
+      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#FC461E] cursor-pointer">
         <Bot className="text-white h-[20px] w-[20px]" />
       </div>
-      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#F95149] ">
+      <div className="flex justify-center items-center h-[40px] w-[40px] bg-[#F95149] cursor-pointer">
         <Heart className="text-white h-[20px] w-[20px]" />
       </div>
     </div>
